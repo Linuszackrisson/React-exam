1c3d1180a2bcc819c589f36b5e617a762e46c701
Linus Zackrisson <linus.gz@hotmail.com>
2024-05-02 03:04:44 +0200

    Details page structure

--- src/components/EventDetails.jsx
+++ src/components/EventDetails.jsx
@@ -1,22 +1,46 @@
-// EventDetails.jsx
-import React from 'react';
+import React, { useState, useEffect } from 'react';
 import { useParams } from 'react-router-dom';
 import { useEventStore } from '../useStore';
+import './eventdetails.css';
 
 function EventDetails() {
-  const { eventId } = useParams(); 
+  const { eventId } = useParams();
   const { events } = useEventStore();
-  const event = events[eventId]; 
+  const event = events[eventId];
+  const [numberOfTickets, setNumberOfTickets] = useState(1);
+  const [totalPrice, setTotalPrice] = useState(event.price); // Initialize total price with the price of one ticket
 
- 
+  // Uppdatera totalpriset när antalet biljetter ändras
+  useEffect(() => {
+    setTotalPrice(event.price * numberOfTickets);
+  }, [numberOfTickets]);
+
+  const addTicket = () => {
+    setNumberOfTickets(prevCount => prevCount + 1);
+  };
+
+  const removeTicket = () => {
+    if (numberOfTickets > 1) {
+      setNumberOfTickets(prevCount => prevCount - 1);
+    }
+  };
 
   return (
     <div className='Event-details'>
+      <h1>Event</h1>
+      <h3>You are about to score <br></br>some tickets to</h3>
       <h2>{event.name}</h2>
       <p>{event.when.date} {event.when.from} - {event.when.to}</p>
-      <p>Datum: {event.when.date}</p>
-      <p>Pris: {event.price} sek</p>
-      {event.where}
+      <p>{event.where}</p>
+      
+       
+        <button onClick={removeTicket}>-</button>
+        {numberOfTickets}
+        <button onClick={addTicket}>+</button>
+      
+      <p>Totalt värde på order</p>
+      <p>{totalPrice} SEK</p>
+      <button className='addToCart'>Lägg till i varukorgen</button>
     </div>
   );
 }
