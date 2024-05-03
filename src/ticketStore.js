37a779c3a65ff3b819d5b251e49a309d9a5b1768
Linus Zackrisson <linus.gz@hotmail.com>
2024-05-03 11:39:06 +0200

    Comments

--- src/ticketStore.js
+++ src/ticketStore.js
@@ -1,8 +1,10 @@
 import { create } from 'zustand';
-
-export const useTicketStore = create((set) => ({
+//Vår andra CustomHook, denna för att hantera kundvagnen
+export const useTicketStore = create((set) => ({ 
   cart: [],
 
+  // Funktionen för att lägga till biljetterna i kundvagnen, inkl en kontroll för att se om eventen redan finns eller ej.
+  // Om det skulle finnas, så adderas antalet biljetter av te.x Lasse Stefanz. Om inte, så skapar vi en ny rad som vanligt.
   addToCart: (event, numberOfTickets) => set((state) => {
     const existingEventIndex = state.cart.findIndex(item => item.event.name === event.name);
     if (existingEventIndex !== -1) {
@@ -15,28 +17,28 @@ export const useTicketStore = create((set) => ({
       };
     }
   }),
-
+ // Funktion för att öka antal biljetter i KUNDVAGNEN (så enbart sådana som har lagt tills)
   increaseTickets: (index) => set((state) => {
     const updatedCart = [...state.cart];
     updatedCart[index].numberOfTickets += 1;
     return { cart: updatedCart };
   }),
-
+// Funktion för att minska antalet biljetter I KUNDVAGNEN
   decreaseTickets: (index) => set((state) => {
     const updatedCart = [...state.cart];
     if (updatedCart[index].numberOfTickets > 1) {
       updatedCart[index].numberOfTickets -= 1;
     } else {
-      updatedCart.splice(index, 1);
+      updatedCart.splice(index, 1); // Men om det enbart är en biljett, och vi sänker till "0" då ryker den helt från kundvagnen.
     }
     return { cart: updatedCart };
   }),
-
+ // Funktion för att ändra antalet biljetter för ett specifikt event i kundvagnen
   handleChangeTickets: (index, value) => set((state) => {
     const updatedCart = [...state.cart];
     updatedCart[index].numberOfTickets = value;
     return { cart: updatedCart };
   }),
 
-  clearCart: () => set({ cart: [] }) // Tömmer varukorgen genom att återställa 'cart' till en tom array
+  clearCart: () => set({ cart: [] }) // Vid köp så andropas denna funktion och vi tömmer kundvagnen.
 }));
