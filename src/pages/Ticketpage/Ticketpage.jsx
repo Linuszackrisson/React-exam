e22b2f9bf2328f08487eea9d8952cda80e7c5950
Linus Zackrisson <linus.gz@hotmail.com>
2024-05-07 01:24:08 +0200

    Last animation

--- src/pages/Ticketpage/Ticketpage.jsx
+++ src/pages/Ticketpage/Ticketpage.jsx
@@ -1,5 +1,6 @@
 import React from 'react';
 import { useOrderStore } from '../../stores/orderStore';
+import { motion } from 'framer-motion'; // Importera motion från framer-motion
 import TicketCard from '../../components/TicketCard/TicketCard';
 import './ticketpage.css';
 
@@ -8,17 +9,23 @@ function Ticketpage() {
 
   return (
     <div className='ticket-page-container'>
-      <div className="ticket-list">
+      <motion.div className="ticket-list" style={{ overflowY: 'hidden' }} initial={{ opacity: 0, y: '-100vh' }} animate={{ opacity: 1, y: 0 }} transition={{ duration: 0.5 }}>
         {orders.map((order, orderIndex) => (
           order.tickets.map((ticket, ticketIndex) => (
-            <TicketCard
+            <motion.div
               key={`${orderIndex}-${ticketIndex}`}
-              order={order}
-              ticket={ticket}
-            />
+              initial={{ opacity: 0, y: 20 }}
+              animate={{ opacity: 1, y: 0 }}
+              transition={{ duration: 1, delay: ticketIndex * 0.1 }}
+            >
+              <TicketCard
+                order={order}
+                ticket={ticket}
+              />
+            </motion.div>
           ))
         ))}
-      </div>
+      </motion.div>
     </div>
   );
 }
